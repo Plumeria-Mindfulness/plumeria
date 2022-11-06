668a9921a34058fd35675100cb77df506f41945f
luizfiller <luiz.fsilva@sptech.school>
2022-11-06 02:47:07 -0300

    Update - Propriedades CSS generalistas, elementos HTML, retirada de JS

--- site/public/js/login.js
+++
@@ -1,9 +0,0 @@
-function verificarSenha() {
-    if (in_senha.type == "password") {
-        in_senha.type = "text";
-        img_olho.src = "assets/eye.png";
-    } else {
-        in_senha.type = "password";
-        img_olho.src = "assets/hidden.png";
-    }
-}
