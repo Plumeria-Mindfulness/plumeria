c5e5d83638e542e8a590bff732c87e4ba696fc57
luizfiller <luiz.fsilva@sptech.school>
2022-11-06 02:47:50 -0300

    Add - HTML, CSS e JS para Cadastro com validação de senha

---
+++ site/public/js/cadastro.js
@@ -0,0 +1,20 @@
+// Função para verificação de senha | Presente na página de Login e de Cadastro
+function verificarSenha() {
+    if (in_senha.type == "password") {
+        in_senha.type = "text";
+        img_olho.src = "assets/eye.png";
+    } else {
+        in_senha.type = "password";
+        img_olho.src = "assets/hidden.png";
+    }
+}
+
+function verificarSenhaConfirmacao() {
+    if (in_confirmacaoSenha.type == "password") {
+        in_confirmacaoSenha.type = "text";
+        img_olhoConfirm.src = "assets/eye.png";
+    } else {
+        in_confirmacaoSenha.type = "password";
+        img_olhoConfirm.src = "assets/hidden.png";
+    }
+}
