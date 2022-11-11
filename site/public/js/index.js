161cb9ecde3e1f7175c7d465e28fb81c572c98f3
luizfiller <luiz.fsilva@sptech.school>
2022-11-11 18:53:00 -0300

    Add - Links p/ section, JS especifico da index, e melhorias de btn e li

---
+++ site/public/js/index.js
@@ -0,0 +1,57 @@
+ScrollReveal().reveal('.content-text',{
+    duration: 1000,
+    distance: "200px",
+})
+
+ScrollReveal().reveal('.historia',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+})
+
+ScrollReveal().reveal('.idealizador',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+    delay: 120,
+})
+
+ScrollReveal().reveal('.funcionalidades',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left"
+})
+
+ScrollReveal().reveal('.descricoes',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+    delay: 800,
+})
+
+ScrollReveal().reveal('.desc-lista-apoio',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+})
+
+ScrollReveal().reveal('.desc-player-musica',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+    delay: 200
+})
+
+ScrollReveal().reveal('.desc-temporizador',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+    delay: 400
+}) 
+
+ScrollReveal().reveal('.desc-dashboard',{
+    duration: 1000,
+    distance: "200px",
+    origin: "left",
+    delay: 600
+}) 
