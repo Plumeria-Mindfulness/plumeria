4dca413052ace9f8aabf7806889a4a1d68baaf7f
luizfiller <luiz.fsilva@sptech.school>
2022-11-11 00:07:29 -0300

    Add - API funcionando completamente até o cadastro!!

--- site/public/js/cadastro.js
+++ site/public/js/cadastro.js
@@ -1,20 +1,20 @@
 // Função para verificação de senha | Presente na página de Login e de Cadastro
-function verificarSenha() {
-    if (in_senha.type == "password") {
-        in_senha.type = "text";
-        img_olhoSenha.src = "assets/eye.png";
-    } else {
-        in_senha.type = "password";
-        img_olhoSenha.src = "assets/hidden.png";
-    }
-}
+// function verificarSenha() {
+//     if (in_senha.type == "password") {
+//         in_senha.type = "text";
+//         img_olhoSenha.src = "assets/eye.png";
+//     } else {
+//         in_senha.type = "password";
+//         img_olhoSenha.src = "assets/hidden.png";
+//     }
+// }
 
-function verificarSenhaConfirmacao() {
-    if (in_confirmacaoSenha.type == "password") {
-        in_confirmacaoSenha.type = "text";
-        img_olhoConfirm.src = "assets/eye.png";
-    } else {
-        in_confirmacaoSenha.type = "password";
-        img_olhoConfirm.src = "assets/hidden.png";
-    }
-}
+// function verificarSenhaConfirmacao() {
+//     if (in_confirmacaoSenha.type == "password") {
+//         in_confirmacaoSenha.type = "text";
+//         img_olhoConfirm.src = "assets/eye.png";
+//     } else {
+//         in_confirmacaoSenha.type = "password";
+//         img_olhoConfirm.src = "assets/hidden.png";
+//     }
+// }
