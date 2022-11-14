de5142e24cb5dca6de174e19b8d7e286fa4b0995
luizfiller <luiz.fsilva@sptech.school>
2022-11-14 14:09:16 -0300

    Update - CSS e HTML do cronômetro e temporizador + JS funções cronômetro

--- site/public/dashboard/dashboard.js
+++ site/public/dashboard/dashboard.js
@@ -1,4 +1,87 @@
-    // Função para criar navbar dropdown
+// Função para passar de relógios
+function passarRelogioCronometro() {
+    subMenuCronometro.style.display = "flex";
+    subMenuTemporizador.style.display = "none";
+}
+
+function passarRelogioTemporizador() {
+    subMenuCronometro.style.display = "none";
+    subMenuTemporizador.style.display = "flex";
+}
+
+// Função para TEMPORIZADOR
+
+
+//  Função para CRÔNOMETRO
+var hora = 0;
+var minuto = 0;
+var segundo = 0;
+
+var tempo = 1000; // quantidade de milisegundos
+var cronometro;
+
+function iniciarCronometro() {
+    cronometro = setInterval(timer, tempo);
+}
+
+function pausarCronometro() {
+    clearInterval(cronometro)
+
+    hora = 0;
+    minuto = 0;
+    segundo = 0;
+
+    document.getElementById('counter_Cronometro').innerHTML = `00 : 00 : 00`;  
+    
+    // ENVIAR VALOR DO CRONÔMETRO PARA O BANCO DE DADOS
+    // ABRIR TELA PARA MENSAGEM/AVALIAÇÃO
+}
+
+function timer() {
+
+    segundo++;
+
+    if (segundo == 60) {
+        segundo = 0;
+        minuto++;
+    
+        if (minuto == 60) {
+            minuto = 0;
+            hora++;
+        }
+    }
+
+
+    var format = (hora < 10 ? '0' + hora : hora) + ' : ' + (minuto < 10 ? '0' + minuto : minuto) + ' : ' +  (segundo < 10 ? '0' + segundo : segundo)
+
+    document.getElementById('counter_Cronometro').innerHTML = format;
+}
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Função para criar navbar dropdown
     var subMenu = document.getElementById("subMenu")
     
     function alternarMenu() {
