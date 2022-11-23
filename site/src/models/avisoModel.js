bc63569fc4e52a3008d8835870c9f9c4698cfc65
luizfiller <luiz.fsilva@sptech.school>
2022-11-23 18:17:45 -0300

    Update - retirada da função pesquisarDescricao

--- site/src/models/avisoModel.js
+++ site/src/models/avisoModel.js
@@ -21,29 +21,6 @@ function listar() {
     return database.executar(instrucao);
 }
 
-function pesquisarDescricao(texto) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
-
-    var instrucao = `
-        SELECT 
-            a.id AS idAviso,
-            a.titulo,
-            a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
-            u.nome,
-            u.email,
-            u.senha
-        FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id
-        WHERE a.descricao LIKE '${texto}';
-    `;
-
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
-}
-
 function listarPorUsuario(idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
 
@@ -70,7 +47,7 @@ function publicar(titulo, descricao, avaliacao, idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", titulo, descricao, avaliacao, idUsuario);
 
     var instrucao = `
-        INSERT INTO mensagem (titulo, descricao, avaliacao, fkUsuario) VALUES ('${titulo}', '${descricao}', '${avaliacao}', ${idUsuario});
+        INSERT INTO mensagem (titulo, descricao, avaliacao, fkUsuario, fkSessao) VALUES ('${titulo}', '${descricao}', '${avaliacao}', ${idUsuario}, (SELECT MAX(idSessao) id FROM sessao where fkUsuario = ${idUsuario}));
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
@@ -91,7 +68,6 @@ function editar(novaDescricao, novaAvaliacao, idAviso) {
 module.exports = {
     listar,
     listarPorUsuario,
-    pesquisarDescricao,
     publicar,
     editar
 }
