f84b75625169bb791501c6bd722c6ab2d4738514
luizfiller <luiz.fsilva@sptech.school>
2022-11-23 23:22:31 -0300

    Add - Estilizção da Lava Lamp, funções JS para passar e sortear cores

--- site/public/dashboard/js/minha-plumeria.js
+++ site/public/dashboard/js/minha-plumeria.js
@@ -1,3 +1,13 @@
+b_usuario.innerHTML = sessionStorage.NOME_USUARIO;
+b_usuarioDropDown.innerHTML = sessionStorage.NOME_USUARIO;
+
+// Função para criar navbar dropdown
+var subMenu = document.getElementById("subMenu")
+
+function alternarMenu() {
+    subMenu.classList.toggle("open-menu")
+}
+
 // Função para habilitar drop down da LISTA DE APOIO
 function mostrarListaApoio() {
     var styleId = document.getElementById("drop_subMenuLista")
@@ -11,7 +21,6 @@ function mostrarListaApoio() {
         drop_subMenuLista.style.display = "none";
     }
 }
-
 
 // Função para habilitar drop down do TEMPORIZADOR
 function mostrarTemporizador() {
@@ -37,6 +46,44 @@ function passarRelogioTemporizador() {
     subMenuCronometro.style.display = "none";
     subMenuTemporizador.style.display = "flex";
 }
+
+// Funções para passar Bubble e Lava Lamp
+function passarBubbleLamp() {
+    containerBubbles.style.display = "none";
+    containerLavaLamp.style.display = "flex";
+}
+
+function passarLampBubble() {
+    containerBubbles.style.display = "flex";
+    containerLavaLamp.style.display = "none";
+}
+
+// Função para sortear cor para Lava Lamp
+function sortearCorLavaLamp() {
+    
+    var cores = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D','#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC','#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399','#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933','#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+    var blobLava = document.getElementsByClassName("blob");
+    
+    var corSorteada = cores[Math.floor(cores.length * Math.random())];
+
+    for (var i = 0; i < blobLava.length; i++) {
+        blobLava[i].style.backgroundColor = corSorteada;
+    };
+}
+
+// Função para sortear cor para Bubble
+function sortearCorBubble() {
+    
+    var cores = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D','#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC','#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399','#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933','#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+    var bubble =  document.querySelector(':root');
+
+    var corSorteada = cores[Math.floor(cores.length * Math.random())];
+    
+    bubble.style.setProperty('--blob', corSorteada);
+}
+
 
 
 // Função para TEMPORIZADOR
@@ -158,17 +205,6 @@ function publicarDuracaoData(totMinFinal, dataString) {
     return false;
 
 }
-
-
-// Função para criar navbar dropdown
-var subMenu = document.getElementById("subMenu")
-
-function alternarMenu() {
-    subMenu.classList.toggle("open-menu")
-}
-
-b_usuario.innerHTML = sessionStorage.NOME_USUARIO;
-b_usuarioDropDown.innerHTML = sessionStorage.NOME_USUARIO;
 
 
 // let proximaAtualizacao;
