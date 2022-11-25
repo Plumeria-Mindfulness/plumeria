df906401673e878e4a248327dfc6cd325db14a3f
luizfiller <luiz.fsilva@sptech.school>
2022-11-24 21:00:00 -0300

    Add - Inserção de redefinir senha no app.js

--- site/app.js
+++ site/app.js
@@ -13,6 +13,7 @@ var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var minhasPlumeriasRouter = require("./src/routes/minhasPlumerias");
+var redefinicaoSenhaRouter = require("./src/routes/redefinicaoSenha");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +26,7 @@ app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/minhasPlumerias", minhasPlumeriasRouter);
+app.use("/redefinicaoSenha", redefinicaoSenhaRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
